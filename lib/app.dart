6cd7ac66f801287f419fdd696602dd377fecd497
Mosab N. R. AbuMoammar <mosab.abumoammar@gmail.com>
2025-02-04 10:52:14 +0200

    launcher icon

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       onGenerateRoute: RouteGenerator.generateRoute,
       initialRoute: RouteName.splashScreen,
     );
